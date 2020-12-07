f1b999e130d64d4539d1152cdaf2b92b03b6538c
espier92 <alex.492@hotmail.com>
2020-12-07 11:43:11 +0100

    RGB msg

--- app.js
+++ app.js
@@ -23,11 +23,42 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
+	
+	cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    
+	
+	
+	var ctx = cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+	var imgData = ctx.getImageData(0, 0, cameraSensor.width, cameraSensor.height);
+
+	// invert colors
+	var sumaPixelsR = 0;
+	var sumaPixelsG = 0;
+	var sumaPixelsB = 0;
+	var contPixels = 0;
+	for (var i = 0; i < imgData.data.length; i += 4) {
+	  sumaPixelsR = sumaPixelsR+imgData.data[i];
+	  sumaPixelsG = sumaPixelsG+imgData.data[i+1];
+	  sumaPixelsB = sumaPixelsB+imgData.data[i+2];
+	  contPixels=contPixels+1;
+	}
+	var mediaPixelsR=sumaPixelsR/contPixels
+	var mediaPixelsG=sumaPixelsG/contPixels
+	var mediaPixelsB=sumaPixelsB/contPixels
+	msg=document.getElementById("message");
+	if (msg.style.display == 'none'){
+		msg.style.display = 'block'
+		msg.innerHTML=mediaPixelsR + ' ; ' +mediaPixelsG + ' ; ' +mediaPixelsB + ' ; '
+	}
+	
+	ctx.putImageData(imgData, 0, 0);
+	
+	
+	cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+    
     // track.stop();
 };
 
